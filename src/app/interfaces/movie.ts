d1a0754ae04696067c49f819e4a1c75f2003005a
Guilherme <guilhermecottamr@hotmail.com>
2024-05-12 18:45:22 -0300

    Alterações na Home Page

--- src/app/interfaces/movie.ts
+++ src/app/interfaces/movie.ts
@@ -1,20 +1,15 @@
-// import { Critic } from "./critic";
+import { Critic } from "./critic";
 
 export interface Movie {
     id: string;
-    title: string;
-    gender: string;
-    release_date: string;
-    // Duração pode ser em minutos e depois formatamos, mas é apenas uma ideia
-    duration: number;
-    // Talvez adicionar um campo que direcione para uma imagem estática da capa do filme
-    // banner: string
-
-    critics: Critic[];
-}
     name: string;
-    description: string;
-    score: number;
     genre: string
     director: string
+    release_date: string;
+    description: string;
+    duration: number; // Duração pode ser em minutos e depois formatamos, mas é apenas uma ideia
+    score: number;
+    critics: Critic[];
+    // Talvez adicionar um campo que direcione para uma imagem estática da capa do filme
+    // banner: string
 }
--- src/app/utils/mock-movies.ts
+++ src/app/utils/mock-movies.ts
@@ -2,27 +2,25 @@ import { Movie } from "../interfaces/movie";
 
 export const MOVIES: Movie[] = [
   {
-    id: "123456",
-    title: "Oppenheimer",
-    gender: "Science",
-    release_date: "07-20-2023",
-    duration: 300,
+    id: "1",
+    name: "Inception",
+    genre: "Sci-Fi",
+    director: "Christopher Nolan",
+    release_date: "2010-07-16",
+    description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+    duration: 148,
+    score: 8.8,
     critics: []
   },
   {
-    id: "1235678",
-    title: "The Dark Knight",
-    gender: "Action",
-    release_date: "07-18-2008",
-    duration: 152,
+    id: "2",
+    name: "The Shawshank Redemption",
+    genre: "Drama",
+    director: "Frank Darabont",
+    release_date: "1994-09-22",
+    description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    duration: 142,
+    score: 9.3,
     critics: []
   }
-    {
-      id: "123456",
-      name: "Oppenheimer",
-      genre: "Science",
-      description: "07-20-2023",
-      score: 4.5,
-      director: "Christopher Nolan"
-    },
 ]
