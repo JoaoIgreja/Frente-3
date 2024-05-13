5244234bb2db8c7306e262ed7d78b292faabfd36
Guilherme <guilhermecottamr@hotmail.com>
2024-05-13 18:02:18 -0300

    Alterações no sistema de listagem

--- src/app/components/movies-list/movies-list.component.ts
+++ src/app/components/movies-list/movies-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Movie } from 'src/app/interfaces/movie';
 import { MovieService } from 'src/app/services/movie.service';
+import { MOVIES } from 'src/app/utils/mock-movies';
 
 @Component({
   selector: 'app-movies-list',
@@ -8,15 +9,16 @@ import { MovieService } from 'src/app/services/movie.service';
   styleUrls: ['./movies-list.component.scss'],
 })
 export class MoviesListComponent {
-  movies: Movie[] = [];
+  movies: Movie[] = MOVIES; // Utiliza as instâncias de filmes mockados
 
   constructor(private movieService: MovieService) { };
 
-  ngOnInit(): void {
+  // Método para buscar os filmes -em comentários para teste-
+  /*ngOnInit(): void {
     this.getMovies();
   }
 
   getMovies(): void {
     this.movieService.getMovies().subscribe(movies => this.movies = movies)
-  }
+  }*/
 }
